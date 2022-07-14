3f22cc4ff9fe05707659c8cb7ac6a845ee54d4fb
Victor <victorslots@gmail.com>
2022-07-13 22:03:19 -0300

    - Adicionada condição de parada do contador - Mudança de cor do número

--- script/script.js
+++ script/script.js
@@ -2,11 +2,21 @@ var currentNumberItem = document.getElementById('CurrentNumber');
 var currentNumber = 0;
 
 function decrementa() {
+    if(currentNumber <= 0 ) {
+        currentNumberberItem = document.getElementById('CurrentNumber').style.color = "red";
+    }else{
+    
     currentNumber = currentNumber - 1;
     currentNumberItem.innerHTML = currentNumber;
+    }
 }
 
 function incrementa() {
+    if(currentNumber >= 10){
+        currentNumberberItem = document.getElementById('CurrentNumber').style.color = "red";
+    }else{
     currentNumber = currentNumber + 1;
     currentNumberItem.innerHTML = currentNumber;
+    currentNumberberItem = document.getElementById('CurrentNumber').style.color = "black";
+    }
 }
